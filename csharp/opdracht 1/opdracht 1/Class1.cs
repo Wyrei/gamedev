bff5ee0d116ea314f7896c6cc02a5e4467a14b75
rubenwkossen <32533@ma-web.nl>
2022-05-09 14:46:46 +0200

    1&2

---
+++ csharp/opdracht 1/opdracht 1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opdracht_1
+{
+    int healt;
+    int name;
+    int energy;
+    class Class1
+    {
+    }
+}
---
+++ csharp/opdracht 1/opdracht 1/Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opdracht_1
+{
+    class skill
+    {
+        int damage;
+        int energycost;
+        int name;
+    }
+}
