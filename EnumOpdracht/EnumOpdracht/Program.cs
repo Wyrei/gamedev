4187acaf7192b650b0e0a99a69ef9bee576baa41
rubenwkossen <32533@ma-web.nl>
2022-06-17 12:34:19 +0200

    1

---
+++ EnumOpdracht/EnumOpdracht/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EnumOpdracht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            colors achtergrondkleur = colors.red;
+            if (achtergrondkleur == colors.green)
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+            }
+        }
+        enum colors
+        {
+            red,
+            green,
+            blue,
+            yellow
+        }
+           
+
+        
+    }
+}
